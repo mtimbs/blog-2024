2d7c7768b80b6dc6234442fa5e16cffcc6a8f0c5
Michael Timbs <michael.timbs@qwilr.com>
2024-09-27 14:59:28 +0930

    basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://michaeltimbs.me",
+  integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "tokyo-night",
+    },
+  },
 });
